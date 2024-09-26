cb2836870705443dd6b74ba3381d01dca40a2aac
Rafael Almeida <rafaelalmeidab@hotmail.com>
2024-09-26 11:44:56 -0300

    feature: updating object state tic tac

---
+++ src/components/GameBoard.jsx
@@ -0,0 +1,41 @@
+import { useState } from 'react'
+
+const initialGameBoard = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
+]
+
+export default function GameBoard() {
+  const [gameBoard, setGameBoard] = useState(initialGameBoard)
+
+  function handleSelectSquare(rowIndex, columnIndex) {
+    setGameBoard((prevGameBoard) => {
+      const updatedBoard = [
+        ...prevGameBoard.map((innerArray) => [...innerArray]),
+      ]
+      updatedBoard[rowIndex][columnIndex] = 'X'
+      return updatedBoard
+    })
+  }
+
+  return (
+    <ol id='game-board'>
+      {gameBoard.map((row, rowIndex) => (
+        <li key={rowIndex}>
+          <ol>
+            {row.map((playerSymbol, columnIndex) => (
+              <li key={columnIndex}>
+                <button
+                  onClick={() => handleSelectSquare(rowIndex, columnIndex)}
+                >
+                  {playerSymbol}
+                </button>
+              </li>
+            ))}
+          </ol>
+        </li>
+      ))}
+    </ol>
+  )
+}
