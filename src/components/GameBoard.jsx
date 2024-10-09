758f8bb6ada75625bc0d7555685db18149d842f6
Rafael Almeida <rafaelalmeidab@hotmail.com>
2024-10-09 05:42:01 -0300

    feature: tic tac toe finished

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,18 +1,4 @@
 export default function GameBoard({ onSelectSquare, board }) {
-  // const [gameBoard, setGameBoard] = useState(initialGameBoard)
-
-  // function handleSelectSquare(rowIndex, columnIndex) {
-  //   setGameBoard((prevGameBoard) => {
-  //     const updatedBoard = [
-  //       ...prevGameBoard.map((innerArray) => [...innerArray]),
-  //     ]
-  //     updatedBoard[rowIndex][columnIndex] = activePlayerSymbol
-  //     return updatedBoard
-  //   })
-
-  //   onSelectSquare()
-  // }
-
   return (
     <ol id="game-board">
       {board.map((row, rowIndex) => (
